fa711b2faac8e19db6941d7771f4ed3340664f10
jeffier.wan <jeffier.wan@ebaotech.com>
2022-06-21 00:25:26 +0800

    import tailwindcss

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,26 @@
 const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight');
+const postcssrc = require('postcss-load-config');
+const postcss = require("postcss");
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPlugin(syntaxHighlight);
+
+    eleventyConfig.addExtension("css", {
+        outputFileExtension: "css",
+        compile: async (content) => {
+            return async () => {
+                const config = await postcssrc({})
+                const result = postcss(config.plugins).process(content, config.options)
+                return result.css
+            }
+        }
+    })
     return {
         dir: {
             input: 'src'
         },
         passthroughFileCopy: true,
-        templateFormats: ['html', 'njk', 'md'],
+        templateFormats: ['html', 'njk', 'md', 'css'],
         htmlTemplateEngine: 'njk',
         markdownTemplateEngine: 'njk'
     }
